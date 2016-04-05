5d9740b6ed17e07353fc236d680b96d97fbc09eb
karen <karenj2105@gmail.com>
2016-04-05 15:56:44 -0500

    Adaptaciones para trabajar con las nuevas hojas de estilo Agregado wilian.css para los estilos que desee agregar wilian durante su labor Cambios en la configuración para quitar el soporte SASS de Ionic Primera subida de las 4 vistas de autenticación: inicio, ingresar-manual, recuperar-contrasena y registrar-manual

--- gulpfile.js
+++ gulpfile.js
@@ -11,25 +11,6 @@ var paths = {
   sass: ['./scss/**/*.scss']
 };
 
-gulp.task('default', ['sass']);
-
-gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
-    .pipe(sass())
-    .on('error', sass.logError)
-    .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
-    .on('end', done);
-});
-
-gulp.task('watch', function() {
-  gulp.watch(paths.sass, ['sass']);
-});
-
 gulp.task('install', ['git-check'], function() {
   return bower.commands.install()
     .on('log', function(data) {
