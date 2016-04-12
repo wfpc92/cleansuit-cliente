c0f96c6047e4781a002f4f5a3f6c1f16ff856863
wfpc92 <wfpc92@hotmail.com>
2016-04-12 13:12:35 -0500

    solucionado checkbox terminos y condiciones, refactorizzacion con los controladores de carrito informacionorden realizarorden

--- www/templates/app/orden/RealizarOrdenCtrl.js
+++ www/templates/app/orden/RealizarOrdenCtrl.js
@@ -1,15 +1,21 @@
-app.controller('RealizarOrdenCtrl', function($scope, RealizarOrdenFactory, $state, $ionicHistory) {
-	//actualizar los datos 
+var RealizarOrdenCtrl = function($scope, RealizarOrdenFactory, $state, $ionicHistory) {
+	var self = this;
+
 	RealizarOrdenFactory.realizarOrden();
 
-	$scope.$on('$ionicView.leave', function(event) {
-
-		//$state.go("app.carrito");
-		$ionicHistory.clearHistory();
-		$ionicHistory.clearCache()
-		$ionicHistory.nextViewOptions({
-			disableBack:'true'
-		});
-		$state.go("app.inicio");
+	$scope.$on('$ionicView.leave', function() {
+		self.viewLeave($state, $ionicHistory);
 	});
-})
+};
+
+RealizarOrdenCtrl.prototype.viewLeave = function($state, $ionicHistory) {
+	//$state.go("app.carrito");
+	$ionicHistory.clearHistory();
+	$ionicHistory.clearCache()
+	$ionicHistory.nextViewOptions({
+		disableBack:'true'
+	});
+	$state.go("app.inicio");
+}
+
+app.controller('RealizarOrdenCtrl', RealizarOrdenCtrl);
