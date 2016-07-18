f39f6b2779da8f065a61e7fb4c33a6db110cfd49
wfpc92 <wfpc92@hotmail.com>
2016-07-18 13:11:35 -0500

    vista de promociones: la etiqueta de descuento ha sido cambiada  para que se acomode segun el numero de descuentos.

--- www/templates/app/principal/AppInicioCtrl.js
+++ www/templates/app/principal/AppInicioCtrl.js
@@ -15,6 +15,38 @@ var AppInicioCtrl = function($scope,
 		$scope.banderas.swp=false;
 		$scope.banderas.sws=false;
 	})
+
+	$scope.generarEtiquetaDescuentos = function(promocion) {
+		//promocion.items = {checked: boolean, descuento: number}
+		var arr = [], etiqueta, cadena;
+		console.log(promocion.items)
+		for(var i in promocion.items){
+			if (arr.indexOf(promocion.items[i].descuento) == -1 && promocion.items[i].descuento) {
+			    arr.push(promocion.items[i].descuento);
+			}
+		}
+
+		if(arr.length == 0){
+			etiqueta = "Descuento del " + promocion.descuento + "%";
+		} else if(arr.length == 1) {
+			etiqueta = "Descuento del " + arr[0] + "%";
+		} else {
+			var cadena = "";
+			for (var i in arr) {
+				cadena += arr[i];
+				console.log(i, arr.length - 1)
+				if(i < arr.length - 2) {
+					cadena += ", ";
+				}
+				if(i == arr.length - 2) {
+				 	cadena += " y "
+				}
+			}
+			etiqueta = "Descuentos del " + cadena + "%";
+		}
+
+		return etiqueta;
+	}
 };
 
 
