13a72a652cc6e5a81422eafb9ee7b82522b7fad7
wfpc92 <wfpc92@hotmail.com>
2016-08-24 19:31:01 -0500

    pendiente: ejecutar la transicion dos veces. mostrar texto

--- www/js/aplicacion/services/Tutorial.js
+++ www/js/aplicacion/services/Tutorial.js
@@ -57,34 +57,78 @@ var TutorialFactory = function($localStorage,
 			}
 
 			if (!imgMano) {
+				var hei = document.getElementById("tutMano").height;
 				mano = angular.element(document.getElementById("tutMano"));
 				texto = angular.element(document.getElementById("tutTexto"));
 				imgMano = $document[0].body.appendChild(mano[0]);
 				imgTexto = $document[0].body.appendChild(texto[0]);
 			}
+
 			$ionicBackdrop.retain();
-			//console.log("TutorialFactory.mostrarTutorial()", tipo)
+
 			var header = angular.element(document.querySelector(".bar-header"))[0];
 			var lst = angular.element(document.querySelector(idLst))[0];
-			var top = (header.offsetHeight + lst.offsetTop + 30) + "px",
-				left = ((1.0 - 0.25) * lst.children[0].offsetWidth) + "px";
+			var top = (header.offsetHeight + lst.offsetTop + 30) ,
+				left = ((1.0 - 0.25) * lst.children[0].offsetWidth);
+				console.log("left:", left)
 
 			if (lst) {
+				
 				mano.addClass("transicionSwipeDerecha");
 				
 				mano.css({
 					"display": "block",
-					"top": top,
+					"top": top + "px", 
 					"position": "absolute",
-					"left": left,
+					"left": left + "px",
 					"-webkit-animation": "transicionSwipeDerecha "+ duracion +"ms ease-in-out",
 					"-webkit-animation-fill-mode": "forwards",
 					"-webkit-transform": "translateX(0)",
 					"z-index": "12"
 				});
+
+				// ubicar el texto en la posicion deseada
+				// se llama a timeout para que pueda acceder a los estilos computados
+				// http://goo.gl/R4JFtm
 				$timeout(function() {
-					self.realizarTutorial(tipo);
-					mano.css("display", "none");
+					var topTexto = top + mano[0].clientHeight + 50,
+						leftTexto = (1.0 - 0.80) * lst.children[0].offsetWidth;
+					
+					texto.css({
+						"display": "block",
+						"top": (topTexto)+ "px",
+						"position": "absolute",
+						"left": leftTexto + "px",
+						"z-index": "12"
+					});
+				})
+
+				//cuando termine la animacion del movimiento, volverlo a hacer
+				$timeout(function() {
+					console.log("removing")
+					mano.removeClass("transicionSwipeDerecha");
+					mano.css({
+						opacity:0,
+						"transition": "opacity 0.3s linear"
+					});
+					$timeout(function(){
+						mano.css({
+							left: left +"px", //79 equivale a la cantidad de pixeles que se corrio en .transicionSwipeDerecha
+							opacity: 1,
+							transition: "opacity 0.3s linear",
+							"-webkit-animation": "none",
+							"-webkit-transform": "none"
+						});
+						/*mano.css({
+							left: (left + 79) +"px", //79 equivale a la cantidad de pixeles que se corrio en .transicionSwipeDerecha
+							opacity: 1,
+							transform: "left 0s linear",
+							transition: "opacity 0.3s linear"
+						});*/
+						//mano.removeClass("notransition");
+						mano.addClass("transicionSwipeDerecha");
+
+					}, 300);
 				}, duracion);
 			}
 			
